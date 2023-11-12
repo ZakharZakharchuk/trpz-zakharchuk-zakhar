86f3fa5acec3f0fedfc9b09bc89e4827a637ee28
Zakhar Zakharchuk <Zakhar_Zakharchuk@epam.com>
2023-11-13 00:06:35 +0200

    Observer pattern impl

---
+++ text-editor/src/main/java/com/example/texteditor/observer/Observer.java
@@ -0,0 +1,6 @@
+package com.example.texteditor.observer;
+
+public interface Observer {
+
+    void update();
+}
---
+++ text-editor/src/main/java/com/example/texteditor/observer/SyntaxHighlight.java
@@ -0,0 +1,33 @@
+package com.example.texteditor.observer;
+
+import com.example.texteditor.Editor;
+import com.example.texteditor.template.AnnotationSyntaxHighlight;
+import com.example.texteditor.template.NumberSyntaxHighlight;
+import com.example.texteditor.template.ReservedWordSyntaxHighlight;
+import com.example.texteditor.template.StringSyntaxHighlight;
+import com.example.texteditor.template.SyntaxHighlightTemplate;
+
+public class SyntaxHighlight implements Observer {
+
+    private final SyntaxHighlightTemplate reservedWordHighlight;
+    private final SyntaxHighlightTemplate stringHighlight;
+    private final SyntaxHighlightTemplate numberHighlight;
+    private final SyntaxHighlightTemplate annotationHighlight;
+
+    public SyntaxHighlight(Editor editor) {
+        reservedWordHighlight = new ReservedWordSyntaxHighlight(editor);
+        stringHighlight = new StringSyntaxHighlight(editor);
+        numberHighlight = new NumberSyntaxHighlight(editor);
+        annotationHighlight = new AnnotationSyntaxHighlight(editor);
+
+        editor.addObserver(this);
+    }
+
+    @Override
+    public void update() {
+        reservedWordHighlight.update();
+        stringHighlight.update();
+        numberHighlight.update();
+        annotationHighlight.update();
+    }
+}
