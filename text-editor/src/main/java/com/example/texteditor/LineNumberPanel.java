6b23da0850276da476bde2f2ac90c1d4f7cd57cd
Zakhar Zakharchuk <Zakhar_Zakharchuk@epam.com>
2023-11-13 00:21:17 +0200

    Line numeration and file command

---
+++ text-editor/src/main/java/com/example/texteditor/LineNumberPanel.java
@@ -0,0 +1,61 @@
+package com.example.texteditor;
+
+import java.awt.Color;
+import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Element;
+
+public class LineNumberPanel extends JPanel {
+
+    private final JTextPane textPane;
+    private final List<Integer> bookmarks;
+
+    public LineNumberPanel(JTextPane textPane, List<Integer> bookmarks) {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.bookmarks = bookmarks;
+        this.textPane = textPane;
+        updateLineNumbers();
+
+        textPane.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateLineNumbers();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateLineNumbers();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateLineNumbers();
+            }
+        });
+    }
+
+    public void updateLineNumbers() {
+        removeAll();
+
+        Element root = textPane.getDocument().getDefaultRootElement();
+        int lineCount = root.getElementCount();
+        int digits = (int) Math.log10(lineCount) + 1;
+
+        for (int i = 1; i <= lineCount; i++) {
+            JLabel lineNumberLabel = new JLabel(String.format("%" + digits + "d", i));
+            if (bookmarks.contains(i)) {
+                lineNumberLabel.setForeground(Color.BLUE);
+            }
+            System.out.println(lineNumberLabel.getForeground());
+            add(lineNumberLabel);
+        }
+
+        revalidate();
+        repaint();
+    }
+}
---
+++ text-editor/src/main/java/com/example/texteditor/command/OpenFileCommand.java
@@ -0,0 +1,32 @@
+package com.example.texteditor.command;
+
+import com.example.texteditor.Editor;
+import com.example.texteditor.strategy.ASCIIStrategy;
+import com.example.texteditor.strategy.EncodingStrategy;
+import com.example.texteditor.strategy.ISOStrategy;
+import com.example.texteditor.strategy.UTFStrategy;
+
+public class OpenFileCommand {
+
+    private final Editor editor;
+
+    public OpenFileCommand(Editor editor) {
+        this.editor = editor;
+    }
+
+    public boolean execute(byte[] fileBytes, String encoding) {
+        EncodingStrategy encodingStrategy;
+        if (encoding.equals("UTF8")) {
+            encodingStrategy = new UTFStrategy();
+        } else if (encoding.equals("US-ASCII")) {
+            encodingStrategy = new ASCIIStrategy();
+        } else if (encoding.equals("ISO-8859-1")) {
+            encodingStrategy = new ISOStrategy();
+        } else {
+            throw new IllegalArgumentException("Unsupported encoding");
+        }
+        String text = encodingStrategy.encode(fileBytes);
+        editor.textPane.setText(text);
+        return false;
+    }
+}
