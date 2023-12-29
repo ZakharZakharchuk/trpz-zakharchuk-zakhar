1446adf24d3c96475962f2be2cb842317dd2ef1c
Zakhar Zakharchuk <Zakhar_Zakharchuk@epam.com>
2023-12-29 22:09:58 +0200

    Refactor service

--- help-service/src/main/java/com/example/helpservice/InfoController.java
+++ help-service/src/main/java/com/example/helpservice/InfoController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/help")
-public class HelpController {
+@RequestMapping("/info")
+public class InfoController {
 
     @GetMapping()
     public String help() {
-        return "help.html";
+        return "info.html";
     }
 }
